5d8fe91d87065d29e1507770a2b12115b59d31e9
jimena campos <jimena@ucol.mx>
2020-11-26 15:34:56 -0600

    costoRenta added

--- src/main.js
+++ src/main.js
@@ -12,6 +12,21 @@ export default class App {
         return resultado;
         
     }
+
+    costoRenta(kilometros){
+        let resultado;
+
+        if (kilometros < 50){
+            resultado = kilometros * 3;
+        } else if (kilometros < 100){
+            resultado = kilometros * 5;
+        } else if (kilometros < 200){
+            resultado = kilometros * 6;
+        } else {
+            resultado = kilometros * 6.5;
+        }
+        return resultado;
+    }
 }
 
 let app = new App();
@@ -19,3 +34,11 @@ let app = new App();
 console.log(app.convertir(100));
 console.log(app.convertir(2000));
 console.log(app.convertir(2100));
+
+console.log(app.costoRenta(25));
+console.log(app.costoRenta(50));
+console.log(app.costoRenta(90));
+console.log(app.costoRenta(100));
+console.log(app.costoRenta(125));
+console.log(app.costoRenta(200));
+console.log(app.costoRenta(250));
