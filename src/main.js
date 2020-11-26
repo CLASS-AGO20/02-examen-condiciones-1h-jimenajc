6e18b80557b9484c961f437e1e4851df6b7f1686
jimena campos <jimena@ucol.mx>
2020-11-26 15:24:55 -0600

    convertir implemented

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 export default class App {
 
+    convertir(pesos){
+        let resultado;
+
+        if( pesos < 2000){
+            resultado = pesos * 20.5;
+        }else {
+            resultado = pesos * 22.7;
+        }
+
+        return resultado;
+        
+    }
 }
 
 let app = new App();
+
+console.log(app.convertir(100));
+console.log(app.convertir(2000));
+console.log(app.convertir(2100));
